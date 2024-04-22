0cad1a8cfaaf6e0f0e9c60cb4c3d476a6790030e
CalinCretu <calincretu1412@gmail.com>
2024-04-22 14:57:58 +0200

    ADD - Utente.cs

--- csharp-biblioteca/Program.cs
+++ csharp-biblioteca/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace csharp_biblioteca
+﻿using csharp_biblioteca;
+using System;
+using static csharp_biblioteca.Utente;
+
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Utente utente1 = new Utente("Rossi", "Mario", "mario@email.com", "password123", "3421458905");
+        Console.WriteLine($"Nome Utente: {utente1.Nome}");
+        Console.WriteLine($"Cognome Utente: {utente1.Cognome}");
+        Console.WriteLine($"Email Utente: {utente1.Email}");
+        Console.WriteLine($"Password Utente: {utente1.Password}");
+        Console.WriteLine($"Recapito Telefonico: {utente1.RecapitoTelefonico}");
     }
 }
---
+++ csharp-biblioteca/Utente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+    public class Utente
+    {
+        public string Cognome { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string RecapitoTelefonico { get; set; }
+
+        // Costruttore per inizializzare un nuovo utente
+        public Utente(string cognome, string nome, string email, string password, string recapitoTelefonico)
+        {
+            // Thread.Sleep(1);    //Per evitare il problema dei random duplicati
+            Cognome = cognome;
+            Nome = nome;
+            Email = email;
+            Password = password;
+            RecapitoTelefonico = recapitoTelefonico;
+        }
+    }
+}
